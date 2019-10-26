00b1346f30757317918f8d3a11371bd62b634dca
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-10-26 18:51:12 +0100

    added program to jobs

--- compile.sh
+++ compile.sh
@@ -11,4 +11,6 @@ MACH_OPTS="-m64"
 # Compile ...
 mpif90 -c ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} -Ifortranlib mod_funcs.F90
 mpif90 -c ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} -Ifortranlib convertBINtoPPM.F90
+mpif90 -c -fopenmp ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} -Ifortranlib createFlood.F90
 mpif90 -o convertBINtoPPM ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} convertBINtoPPM.o fortranlib/*.o -Lfortranlib
+mpif90 -o createFlood -fopenmp ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} createFlood.o mod_funcs.o fortranlib/*.o -Lfortranlib
