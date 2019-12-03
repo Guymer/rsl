9ad7f99614421fe214c28958ff4e9203aa2bc3dc
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-12-03 19:25:10 +0000

    creates full resolution GIF and downsized GIFs

--- convertPNGtoGIF.py
+++ convertPNGtoGIF.py
@@ -15,8 +15,7 @@ try:
 except:
     raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH")
 
-# Set scale ...
-scale = 2
+# ******************************************************************************
 
 # Initizalize list ...
 images = []
@@ -26,16 +25,38 @@ for frame in sorted(glob.glob("createFlood_0[0-4][0-9][0-9]m.png")):
     # Open image as RGB (even if it is paletted) ...
     image = PIL.Image.open(frame).convert("RGB")
 
-    # Check that it can be scaled correctly ...
-    if image.size[0] % scale != 0:
-        raise Exception("width is not an integer multiple of \"scale\"")
-    if image.size[1] % scale != 0:
-        raise Exception("height is not an integer multiple of \"scale\"")
-
-    # Downscale the image and append it to the list ...
-    images.append(image.resize((image.size[0] // scale, image.size[1] // scale), resample = PIL.Image.LANCZOS))
+    # Append it to the list ...
+    images.append(image)
 
 # Save GIF ...
 images[0].save("createFlood.gif", save_all = True, append_images = images[1:], duration = 40, loop = 0)
 pyguymer3.exiftool("createFlood.gif")
 pyguymer3.gifsicle("createFlood.gif")
+
+# ******************************************************************************
+
+# Set widths ...
+# NOTE: By inspection, the PNG frames are 1320 wide.
+widths = [512, 1024]                                                            # [px]
+
+# Loop over widths ...
+for width in widths:
+    # Initizalize list ...
+    images = []
+
+    # Loop over frames (0,000m to 0,499m) ...
+    for frame in sorted(glob.glob("createFlood_0[0-4][0-9][0-9]m.png")):
+        # Open image as RGB (even if it is paletted) ...
+        image = PIL.Image.open(frame).convert("RGB")
+
+        # Calculate height ...
+        ratio = float(image.size[0]) / float(image.size[1])                     # [px/px]
+        height = round(float(width) / ratio)                                    # [px]
+
+        # Downscale the image and append it to the list ...
+        images.append(image.resize((width, height), resample = PIL.Image.LANCZOS))
+
+    # Save GIF ...
+    images[0].save("createFlood{:04d}px.gif".format(width), save_all = True, append_images = images[1:], duration = 40, loop = 0)
+    pyguymer3.exiftool("createFlood{:04d}px.gif".format(width))
+    pyguymer3.gifsicle("createFlood{:04d}px.gif".format(width))
