60562ffe606cb6ec68b81a5c9604e7fca2c2b205
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-12-19 15:44:47 +0000

    added header

--- convertPPMtoPNG.sh
+++ convertPPMtoPNG.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# Check that non-standard programs are installed. "standard" programs are
+# anything that is specified in the POSIX.1-2008 standard (and the IEEE Std
+# 1003.1 standard) or that is a BASH builtin command. Therefore, "non-standard"
+# programs are anything that does not appear on the following two lists:
+#   * https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
+#   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+if ! type convert &> /dev/null; then
+    echo "ERROR: \"convert\" is not installed." >&2
+    exit 1
+fi
+if ! type exiftool &> /dev/null; then
+    echo "ERROR: \"exiftool\" is not installed." >&2
+    exit 1
+fi
+if ! type optipng &> /dev/null; then
+    echo "ERROR: \"optipng\" is not installed." >&2
+    exit 1
+fi
+
 # Convert dataset map ...
 ppm="terr50_gagg_gb.ppm"
 png="terr50_gagg_gb.png"
