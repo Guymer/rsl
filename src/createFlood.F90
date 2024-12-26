659277645708377d0c7e2e1773fdc2f832ca4eca
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-12-26 13:51:42 +0000

    use new subroutine from "mod_safe"

--- src/createFlood.F90
+++ src/createFlood.F90
@@ -1,45 +1,32 @@
 PROGRAM main
-    ! Import modules ...
+    ! Import standard modules ...
     USE ISO_FORTRAN_ENV
-    USE mod_funcs
+
+    ! Import my modules ...
+    USE mod_funcs,          ONLY:   saveShrunkFlood
     USE mod_safe,           ONLY:   sub_allocate_array,                         &
+                                    sub_flood_array,                            &
                                     sub_load_array_from_BIN
 
     IMPLICIT NONE
 
     ! Declare parameters ...
-    INTEGER(kind = INT64), PARAMETER                                            :: nmax = 50_INT64
-    INTEGER(kind = INT64), PARAMETER                                            :: nmaxThread = 1000_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: nx = 13200_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: ny = 24600_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: imageScale = 10_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: tileScale = 50_INT64
 
     ! Declare variables ...
-    CHARACTER(len = 19)                                                         :: cname
     CHARACTER(len = 19)                                                         :: iname
     LOGICAL(kind = INT8), ALLOCATABLE, DIMENSION(:, :)                          :: atRisk
     LOGICAL(kind = INT8), ALLOCATABLE, DIMENSION(:, :)                          :: flooded
-    INTEGER(kind = INT64)                                                       :: i
     INTEGER(kind = INT64)                                                       :: iSeaLevel
-    INTEGER(kind = INT64)                                                       :: iThread
-    INTEGER(kind = INT64)                                                       :: ix
-    INTEGER(kind = INT64)                                                       :: ixlo
-    INTEGER(kind = INT64)                                                       :: ixhi
-    INTEGER(kind = INT64)                                                       :: iy
-    INTEGER(kind = INT64)                                                       :: iylo
-    INTEGER(kind = INT64)                                                       :: iyhi
-    INTEGER(kind = INT64)                                                       :: newtot
-    INTEGER(kind = INT64)                                                       :: newtotThread
-    INTEGER(kind = INT64)                                                       :: oldtot
-    INTEGER(kind = INT64)                                                       :: oldtotThread
     REAL(kind = REAL32)                                                         :: seaLevel
     REAL(kind = REAL32), ALLOCATABLE, DIMENSION(:, :)                           :: elev
 
     ! Declare FORTRAN variables ...
     CHARACTER(len = 256)                                                        :: errmsg
     INTEGER(kind = INT32)                                                       :: errnum
-    INTEGER(kind = INT32)                                                       :: funit
 
     ! Check imageScale ...
     IF(MOD(nx, imageScale) /= 0_INT64)THEN
@@ -49,18 +36,6 @@ PROGRAM main
     END IF
     IF(MOD(ny, imageScale) /= 0_INT64)THEN
         WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "imageScale"'
-        FLUSH(unit = ERROR_UNIT)
-        STOP
-    END IF
-
-    ! Check tileScale ...
-    IF(MOD(nx, tileScale) /= 0_INT64)THEN
-        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "tileScale"'
-        FLUSH(unit = ERROR_UNIT)
-        STOP
-    END IF
-    IF(MOD(ny, tileScale) /= 0_INT64)THEN
-        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "tileScale"'
         FLUSH(unit = ERROR_UNIT)
         STOP
     END IF
@@ -80,11 +55,10 @@ PROGRAM main
     ! Allocate (309.68 MiB) array ...
     CALL sub_allocate_array(atRisk, "atRisk", nx, ny, .TRUE._INT8)
 
-    ! Allocate (309.68 MiB) array and initialize it so that nowhere is flooded ...
+    ! Allocate (309.68 MiB) array and initialize it so that nowhere is flooded
+    ! apart from the top-left corner ...
     CALL sub_allocate_array(flooded, "flooded", nx, ny, .TRUE._INT8)
     flooded = .FALSE._INT8
-
-    ! Flood the top-left corner ...
     flooded(1, 1) = .TRUE._INT8
 
     ! Loop over sea levels ...
@@ -97,109 +71,18 @@ PROGRAM main
         seaLevel = REAL(iSeaLevel, kind = REAL32)                               ! [m]
         atRisk = elev <= seaLevel
 
-        ! Create file names ...
-        WRITE(cname, '("../output/", i4.4, "m.csv")') iSeaLevel
+        ! Raise the sea level ...
+        CALL sub_flood_array(                                                   &
+                   nx = nx,                                                     &
+                   ny = ny,                                                     &
+                 elev = elev,                                                   &
+             seaLevel = seaLevel,                                               &
+              flooded = flooded,                                                &
+            tileScale = tileScale                                               &
+        )
+
+        ! Create file name and save shrunk final flood ...
         WRITE(iname, '("../output/", i4.4, "m.ppm")') iSeaLevel
-
-        ! Open CSV ...
-        OPEN(action = "write", file = cname, form = "formatted", iomsg = errmsg, iostat = errnum, newunit = funit, status = "replace")
-        IF(errnum /= 0_INT32)THEN
-            WRITE(fmt = '("ERROR: ", a, ". ERRMSG = ", a, ". ERRNUM = ", i3, ".")', unit = ERROR_UNIT) "Failed to open BIN", TRIM(errmsg), errnum
-            FLUSH(unit = ERROR_UNIT)
-            STOP
-        END IF
-
-        ! Write header ...
-        WRITE(fmt = '(a)', unit = funit) "step,pixels flooded"
-        FLUSH(unit = funit)
-
-        ! Start ~infinite loop ...
-        DO i = 1_INT64, nmax
-            ! Print progress ...
-            WRITE(fmt = '("    Calculating step ", i4, " of (up to) ", i4, " ...")', unit = OUTPUT_UNIT) i, nmax
-            FLUSH(unit = OUTPUT_UNIT)
-
-            ! Find initial total ...
-            oldtot = COUNT(flooded, kind = INT64)
-
-            ! Increment flood (of GB) ...
-            CALL incrementFlood(nx, ny, elev, seaLevel, flooded, 1_INT64, nx, 1_INT64, ny)
-
-            !$omp parallel                                                      &
-            !$omp default(none)                                                 &
-            !$omp private(iThread)                                              &
-            !$omp private(ix)                                                   &
-            !$omp private(ixlo)                                                 &
-            !$omp private(ixhi)                                                 &
-            !$omp private(iy)                                                   &
-            !$omp private(iylo)                                                 &
-            !$omp private(iyhi)                                                 &
-            !$omp private(oldtotThread)                                         &
-            !$omp private(newtotThread)                                         &
-            !$omp shared(elev)                                                  &
-            !$omp shared(flooded)                                               &
-            !$omp shared(seaLevel)
-                !$omp do                                                        &
-                !$omp schedule(dynamic)
-                    ! Loop over x-axis tiles ...
-                    DO ix = 1_INT64, nx / tileScale
-                        ! Find the extent of the tile ...
-                        ixlo = (ix - 1_INT64) * tileScale + 1_INT64
-                        ixhi =  ix            * tileScale
-
-                        ! Loop over y-axis tiles ...
-                        DO iy = 1_INT64, ny / tileScale
-                            ! Find the extent of the tile ...
-                            iylo = (iy - 1_INT64) * tileScale + 1_INT64
-                            iyhi =  iy            * tileScale
-
-                            ! Start ~infinite loop ...
-                            DO iThread = 1_INT64, nmaxThread
-                                ! Find initial total ...
-                                oldtotThread = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
-
-                                ! Stop looping once no more flooding can occur ...
-                                IF(oldtotThread == 0_INT64 .OR. oldtotThread == tileScale ** 2)THEN
-                                    EXIT
-                                END IF
-
-                                ! Increment flood (of the tile) ...
-                                CALL incrementFlood(nx, ny, elev, seaLevel, flooded, ixlo, ixhi, iylo, iyhi)
-
-                                ! Find new total ...
-                                newtotThread = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
-
-                                ! Stop looping once no more flooding has occured ...
-                                IF(newtotThread == oldtotThread)THEN
-                                    EXIT
-                                END IF
-                            END DO
-                        END DO
-                    END DO
-                !$omp end do
-            !$omp end parallel
-
-            ! Find new total ...
-            newtot = COUNT(flooded, kind = INT64)
-
-            ! Write progress ...
-            WRITE(fmt = '(i2, ",", i9)', unit = funit) i, newtot
-            FLUSH(unit = funit)
-
-            ! Stop looping once no more flooding has occured ...
-            IF(newtot == oldtot)THEN
-                EXIT
-            END IF
-        END DO
-
-        ! Close CSV ...
-        CLOSE(unit = funit)
-
-        ! Print progress ...
-        WRITE(fmt = '("    Saving final answer ...")', unit = OUTPUT_UNIT)
-        FLUSH(unit = OUTPUT_UNIT)
-
-        ! Save shrunk final flood ...
         CALL saveShrunkFlood(nx, ny, atRisk, flooded, imageScale, iname)
     END DO
 
--- src/mod_funcs.F90
+++ src/mod_funcs.F90
@@ -1,169 +1,6 @@
 MODULE mod_funcs
     CONTAINS
 
-    PURE SUBROUTINE incrementFlood(nx, ny, elev, seaLevel, flooded, ixlo, ixhi, iylo, iyhi)
-        ! Import modules ...
-        USE ISO_FORTRAN_ENV
-
-        IMPLICIT NONE
-
-        ! Declare inputs/outputs ...
-        INTEGER(kind = INT64), INTENT(in)                                       :: nx
-        INTEGER(kind = INT64), INTENT(in)                                       :: ny
-        REAL(kind = REAL32), DIMENSION(nx, ny), INTENT(inout)                   :: elev
-        REAL(kind = REAL32), INTENT(in)                                         :: seaLevel
-        LOGICAL(kind = INT8), DIMENSION(nx, ny), INTENT(inout)                  :: flooded
-        INTEGER(kind = INT64), INTENT(in)                                       :: ixlo
-        INTEGER(kind = INT64), INTENT(in)                                       :: ixhi
-        INTEGER(kind = INT64), INTENT(in)                                       :: iylo
-        INTEGER(kind = INT64), INTENT(in)                                       :: iyhi
-
-        ! Declare variables ...
-        INTEGER(kind = INT64)                                                   :: ix
-        INTEGER(kind = INT64)                                                   :: ix1
-        INTEGER(kind = INT64)                                                   :: ix2
-        INTEGER(kind = INT64)                                                   :: iy
-        INTEGER(kind = INT64)                                                   :: iy1
-        INTEGER(kind = INT64)                                                   :: iy2
-
-        ! NOTE: The arrays go:
-        !       ( 1, 1) ... (nx, 1)
-        !         ...         ...
-        !       ( 1,ny) ... (nx,ny)
-
-        ! **********************************************************************
-        ! NOTE: Start top-left and go down then right.
-        ! **********************************************************************
-
-        ! Loop over x-axis ...
-        DO ix = ixlo, ixhi, 1_INT64
-            ! Find the limits of the border around this pixel ...
-            ix1 = MAX(ix - 1_INT64, ixlo)
-            ix2 = MIN(ix + 1_INT64, ixhi)
-
-            ! Loop over y-axis ...
-            DO iy = iylo, iyhi, 1_INT64
-                ! Find the limits of the border around this pixel ...
-                iy1 = MAX(iy - 1_INT64, iylo)
-                iy2 = MIN(iy + 1_INT64, iyhi)
-
-                ! Check that this pixel has not already been flooded ...
-                IF(.NOT. flooded(ix, iy))THEN
-                    ! Check that this pixel is <= sea level ...
-                    IF(elev(ix, iy) <= seaLevel)THEN
-                        ! Check that this pixel is next to a flooded pixel ...
-                        IF(ANY(flooded(ix1:ix2, iy1:iy2)))THEN
-                            ! Flood this pixel ...
-                            ! NOTE: Within flooded:
-                            !         * .FALSE. = not flooded
-                            !         * .TRUE.  = flooded
-                            flooded(ix, iy) = .TRUE._INT8
-                        END IF
-                    END IF
-                END IF
-            END DO
-        END DO
-
-        ! **********************************************************************
-        ! NOTE: Start top-right and go left then down.
-        ! **********************************************************************
-
-        ! Loop over y-axis ...
-        DO iy = iylo, iyhi, 1_INT64
-            ! Find the limits of the border around this pixel ...
-            iy1 = MAX(iy - 1_INT64, iylo)
-            iy2 = MIN(iy + 1_INT64, iyhi)
-
-            ! Loop over x-axis ...
-            DO ix = ixhi, ixlo, -1_INT64
-                ! Find the limits of the border around this pixel ...
-                ix1 = MAX(ix - 1_INT64, ixlo)
-                ix2 = MIN(ix + 1_INT64, ixhi)
-
-                ! Check that this pixel has not already been flooded ...
-                IF(.NOT. flooded(ix, iy))THEN
-                    ! Check that this pixel is <= sea level ...
-                    IF(elev(ix, iy) <= seaLevel)THEN
-                        ! Check that this pixel is next to a flooded pixel ...
-                        IF(ANY(flooded(ix1:ix2, iy1:iy2)))THEN
-                            ! Flood this pixel ...
-                            ! NOTE: Within flooded:
-                            !         * .FALSE. = not flooded
-                            !         * .TRUE.  = flooded
-                            flooded(ix, iy) = .TRUE._INT8
-                        END IF
-                    END IF
-                END IF
-            END DO
-        END DO
-
-        ! **********************************************************************
-        ! NOTE: Start bottom-right and go up then left.
-        ! **********************************************************************
-
-        ! Loop over x-axis ...
-        DO ix = ixhi, ixlo, -1_INT64
-            ! Find the limits of the border around this pixel ...
-            ix1 = MAX(ix - 1_INT64, ixlo)
-            ix2 = MIN(ix + 1_INT64, ixhi)
-
-            ! Loop over y-axis ...
-            DO iy = iyhi, iylo, -1_INT64
-                ! Find the limits of the border around this pixel ...
-                iy1 = MAX(iy - 1_INT64, iylo)
-                iy2 = MIN(iy + 1_INT64, iyhi)
-
-                ! Check that this pixel has not already been flooded ...
-                IF(.NOT. flooded(ix, iy))THEN
-                    ! Check that this pixel is <= sea level ...
-                    IF(elev(ix, iy) <= seaLevel)THEN
-                        ! Check that this pixel is next to a flooded pixel ...
-                        IF(ANY(flooded(ix1:ix2, iy1:iy2)))THEN
-                            ! Flood this pixel ...
-                            ! NOTE: Within flooded:
-                            !         * .FALSE. = not flooded
-                            !         * .TRUE.  = flooded
-                            flooded(ix, iy) = .TRUE._INT8
-                        END IF
-                    END IF
-                END IF
-            END DO
-        END DO
-
-        ! **********************************************************************
-        ! NOTE: Start bottom-left and go right then up.
-        ! **********************************************************************
-
-        ! Loop over y-axis ...
-        DO iy = iyhi, iylo, -1_INT64
-            ! Find the limits of the border around this pixel ...
-            iy1 = MAX(iy - 1_INT64, iylo)
-            iy2 = MIN(iy + 1_INT64, iyhi)
-
-            ! Loop over x-axis ...
-            DO ix = ixlo, ixhi, 1_INT64
-                ! Find the limits of the border around this pixel ...
-                ix1 = MAX(ix - 1_INT64, ixlo)
-                ix2 = MIN(ix + 1_INT64, ixhi)
-
-                ! Check that this pixel has not already been flooded ...
-                IF(.NOT. flooded(ix, iy))THEN
-                    ! Check that this pixel is <= sea level ...
-                    IF(elev(ix, iy) <= seaLevel)THEN
-                        ! Check that this pixel is next to a flooded pixel ...
-                        IF(ANY(flooded(ix1:ix2, iy1:iy2)))THEN
-                            ! Flood this pixel ...
-                            ! NOTE: Within flooded:
-                            !         * .FALSE. = not flooded
-                            !         * .TRUE.  = flooded
-                            flooded(ix, iy) = .TRUE._INT8
-                        END IF
-                    END IF
-                END IF
-            END DO
-        END DO
-    END SUBROUTINE incrementFlood
-
     SUBROUTINE saveArray(shrunkAtRisk, shrunkFlooded, fname)
         ! NOTE: This is a copy of "sub_save_2D_REAL32_real_array_as_PPM()" from
         !       "mod_safe", the only modification is to be hard-coded to be
@@ -250,7 +87,7 @@ MODULE mod_funcs
         DEALLOCATE(img)
     END SUBROUTINE saveArray
 
-    SUBROUTINE saveShrunkFlood(nx, ny, atRisk, flooded, scale, iname)
+    SUBROUTINE saveShrunkFlood(nx, ny, atRisk, flooded, imageScale, iname)
         ! Import modules ...
         USE ISO_FORTRAN_ENV
         USE mod_safe,       ONLY:   sub_allocate_array
@@ -262,7 +99,7 @@ MODULE mod_funcs
         INTEGER(kind = INT64), INTENT(in)                                       :: ny
         LOGICAL(kind = INT8), DIMENSION(nx, ny), INTENT(in)                     :: atRisk
         LOGICAL(kind = INT8), DIMENSION(nx, ny), INTENT(in)                     :: flooded
-        INTEGER(kind = INT64), INTENT(in)                                       :: scale
+        INTEGER(kind = INT64), INTENT(in)                                       :: imageScale
         CHARACTER(len = *), INTENT(in)                                          :: iname
 
         ! Declare variables ...
@@ -275,44 +112,44 @@ MODULE mod_funcs
         REAL(kind = REAL32), ALLOCATABLE, DIMENSION(:, :)                       :: shrunkAtRisk
         REAL(kind = REAL32), ALLOCATABLE, DIMENSION(:, :)                       :: shrunkFlooded
 
-        ! Check scale ...
-        IF(MOD(nx, scale) /= 0_INT64)THEN
-            WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "scale"'
+        ! Check imageScale ...
+        IF(MOD(nx, imageScale) /= 0_INT64)THEN
+            WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "imageScale"'
             FLUSH(unit = ERROR_UNIT)
             STOP
         END IF
-        IF(MOD(ny, scale) /= 0_INT64)THEN
-            WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "scale"'
+        IF(MOD(ny, imageScale) /= 0_INT64)THEN
+            WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "imageScale"'
             FLUSH(unit = ERROR_UNIT)
             STOP
         END IF
 
         ! Allocate array ...
-        CALL sub_allocate_array(shrunkAtRisk, "shrunkAtRisk", nx / scale, ny / scale, .TRUE._INT8)
-        CALL sub_allocate_array(shrunkFlooded, "shrunkFlooded", nx / scale, ny / scale, .TRUE._INT8)
+        CALL sub_allocate_array(shrunkAtRisk, "shrunkAtRisk", nx / imageScale, ny / imageScale, .TRUE._INT8)
+        CALL sub_allocate_array(shrunkFlooded, "shrunkFlooded", nx / imageScale, ny / imageScale, .TRUE._INT8)
 
         ! Loop over x-axis tiles ...
-        DO ix = 1_INT64, nx / scale
+        DO ix = 1_INT64, nx / imageScale
             ! Find the extent of the tile ...
-            ixlo = (ix - 1_INT64) * scale + 1_INT64
-            ixhi =  ix            * scale
+            ixlo = (ix - 1_INT64) * imageScale + 1_INT64
+            ixhi =  ix            * imageScale
 
             ! Loop over y-axis tiles ...
-            DO iy = 1_INT64, ny / scale
+            DO iy = 1_INT64, ny / imageScale
                 ! Find the extent of the tile ...
-                iylo = (iy - 1_INT64) * scale + 1_INT64
-                iyhi =  iy            * scale
+                iylo = (iy - 1_INT64) * imageScale + 1_INT64
+                iyhi =  iy            * imageScale
 
                 ! Find total risk ...
                 ! NOTE: Within shrunkAtRisk:
-                !         *     0.0     = not at risk = GREEN/BLUE
-                !         * scale*scale =   at risk   = RED
+                !         *          0.           = not at risk = GREEN/BLUE
+                !         * imageScale*imageScale =   at risk   = RED
                 shrunkAtRisk(ix, iy) = REAL(COUNT(atRisk(ixlo:ixhi, iylo:iyhi), kind = INT64), kind = REAL32)
 
                 ! Find total flood ...
                 ! NOTE: Within shrunkFlooded:
-                !         *     0.0     = not flooded = GREEN/RED
-                !         * scale*scale =   flooded   = BLUE
+                !         *          0.0          = not flooded = GREEN/RED
+                !         * imageScale*imageScale =   flooded   = BLUE
                 shrunkFlooded(ix, iy) = REAL(COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64), kind = REAL32)
             END DO
         END DO
@@ -321,13 +158,13 @@ MODULE mod_funcs
         ! NOTE: Within shrunkAtRisk:
         !         * 0.0 = not at risk = GREEN/BLUE
         !         * 1.0 =   at risk   = RED
-        shrunkAtRisk = shrunkAtRisk / REAL(scale * scale, kind = REAL32)
+        shrunkAtRisk = shrunkAtRisk / REAL(imageScale * imageScale, kind = REAL32)
 
         ! Convert total flood to average flood ...
         ! NOTE: Within shrunkFlooded:
         !         * 0.0 = not flooded = GREEN/RED
         !         * 1.0 =   flooded   = BLUE
-        shrunkFlooded = shrunkFlooded / REAL(scale * scale, kind = REAL32)
+        shrunkFlooded = shrunkFlooded / REAL(imageScale * imageScale, kind = REAL32)
 
         ! Save shrunk flood ...
         CALL saveArray(shrunkAtRisk, shrunkFlooded, iname)
